05116fee577f47c791b7f9940c492233d3e70173
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-15 14:54:43 +0600

    fake data added

---
+++ fakedata.js
@@ -0,0 +1,29 @@
+export const users =[
+    {
+        firstname:'mim',
+        roll:1450132,
+        identity:'student',
+        email:'mim.cse@gmail.com',
+        pass:'123'
+    },
+    {
+        firstname:'hakkani',
+        roll:1450133,
+        identity:'employee',
+        email:'hakkani.cse@gmail.com',
+        pass:'1234'
+    },
+
+]
+
+export const quotes =[
+    {
+        name:'allah is on with us ',
+        by:12354
+    },
+     {
+        name:'allah is see all of us ',
+        by:1111
+    },
+
+]
