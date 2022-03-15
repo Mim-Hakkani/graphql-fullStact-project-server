4bfe9c4bd8e71b7bc1c47b7f57ef14192418518b
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-15 13:18:02 +0600

    create a simple schema and resolver s

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 import { gql,ApolloServer } from "apollo-server";
 import {ApolloServerPluginLandingPageGraphQLPlayground} from  "apollo-server-core"
+
+//create a simple schema 
+
+const typeDefs =gql`
+type Query{
+    greet : String
+}
+
+`
+
+//create a  resolvers for calculations in query ,mutaion ,and subscriptions 
+
+const resolvers = {
+    Query:{
+        greet:()=>"hellow world"
+    }
+}
